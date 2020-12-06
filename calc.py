41cdea749f1fdb1e51f17eeea04fe71e94f6cc91
morimzu <mizu.mizupica@gmail.com>
2020-12-06 10:07:04 +0900

    画像のサイズから偏差もcsvに登録できるようにした

--- calc.py
+++ calc.py
@@ -5,10 +5,12 @@ import pandas as pd
 df = pd.read_csv("data/Determination_factors.csv")
 fact = df.values.tolist()
 
-def calc(width):
-    import numpy
-    x = numpy.roots([fact[0][0], fact[0][1], fact[0][2], fact[0][3], fact[0][4], fact[0][5]-width])
+def calc_dist(width):
+    x = np.roots([fact[0][0], fact[0][1], fact[0][2]-width])
     print(x)
     for dist in x:
         if dist >= 30.0 and dist <= 100.0 and dist.imag == 0:
             return dist.real
+
+def calc_devi():
+    pass
