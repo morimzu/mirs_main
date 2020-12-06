6f944833bf1d1a5c55aca007f2b088aa3f41d5c7
morimzu <mizu.mizupica@gmail.com>
2020-12-06 12:18:24 +0900

    偏差を求める式の変更

--- calc.py
+++ calc.py
@@ -15,7 +15,7 @@ def calc_dist(width):
     raise Exception("The distance is out of range.")
 
 def calc_devi(devi):
-    y = np.roots([fact[0][0]/27.5, fact[0][1]/27.5, fact[0][2]/27.5, (fact[0][3]-devi)/27.5])
+    y = np.roots([fact[0][0]/27.5, fact[0][1]/27.5, fact[0][2]/27.5, fact[0][3]/27.5-devi])
     print(y)
     for devi in y:
         if devi >=-100 and devi <= 100 and devi.imag == 0:
