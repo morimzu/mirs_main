4a9174f2c8535463abcc1703d927202fdf1a85f4
morimzu <mizu.mizupica@gmail.com>
2020-12-09 16:09:27 +0900

    超音波センサ関係のプログラム作成

--- io.py
+++ io.py
@@ -4,9 +4,9 @@ import smbus
 #SMBusの引数に1を指定する。Raspberry Piのi2cバスの番号
 i2c = smbus.SMBus(1)
 #デバイスのアドレス 0x68
-addr = 0x68
+addr = 0x72
 
 if __name__ == "__main__":
     while True:
-        data = i2c.read_byte_data(addr, 0x72)
+        data = i2c.read_byte_data(addr, 0x05)
         print(data)
