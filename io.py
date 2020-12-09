0c45612df0f5c774eea616a0b92bba7014dd1777
morimzu <mizu.mizupica@gmail.com>
2020-12-09 16:22:28 +0900

    超音波センサのプログラム追加

--- io.py
+++ io.py
@@ -1,13 +1,27 @@
 import time
 import smbus
 
-#SMBusの引数に1を指定する。Raspberry Piのi2cバスの番号
 i2c = smbus.SMBus(1)
-#デバイスのアドレス 0x68
-addr = 0x72
+address = 0x72
+SLEEPTIME = 0.5
+DELAY = 0.1
 
-if __name__ == "__main__":
-    while True:
-        data = i2c.read_byte_data(addr, 0x51)
-        print(data)
-        time.sleep(1.0)
+if __name__ == '__main__':
+    try:
+        print ("cancel：CTRL+C")
+        cnt = 1
+        while True:
+            i2c.write_i2c_block_data(address,0x00,[0x51])
+            # 0x51が測定コマンド 51で単位がセンチメートル
+            time.sleep(DELAY)
+
+            block = i2c.read_i2c_block_data(address,0x00,4)
+            distance = (block[2]<<8 | block[3])
+            print(int(cnt),distance)
+            cnt = cnt + 1
+            time.sleep(SLEEPTIME)
+
+    except KeyboardInterrupt:
+        print("finishing...")
+    finally:
+        print("finish!!")
