f9283d65f7a9aff438ef572cde215ed5fcca436f
morimzu <mizu.mizupica@gmail.com>
2020-12-15 12:50:15 +0900

    テスト用プログラムの追加

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 for i in range(10):
         ser.write(str.encode('a'))
         print("LED ON")
-        time.sleep(0.5)
+        time.sleep(2)
         ser.write(str.encode('0'))
         print("LED OFF")
-        time.sleep(0.5)
+        time.sleep(2)
 
 ser.close()
 print("bbb")
