e9098fb1c95e30359a99dec5007efb07635fb308
morimzu <mizu.mizupica@gmail.com>
2020-12-15 12:44:23 +0900

    テスト用プログラムの追加

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-import pandas as pd
+import serial
+import time
+ 
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
-df = pd.read_csv("ObjectDistanceDatas.csv")
-print(df)
+for i in range(10):
+        ser.write('a')
+        print("LED ON")
+        time.sleep(0.5)
+        ser.write('0')
+        print("LED OFF")
+        time.sleep(0.5)
+
+ser.close()
+print("bbb")
