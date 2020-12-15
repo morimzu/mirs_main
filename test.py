cae03c5285bcfe2d09e6633efe2693a2409c97e6
morimzu <mizu.mizupica@gmail.com>
2020-12-15 12:48:25 +0900

    テスト用プログラムの追加

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import serial
 import time
- 
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 for i in range(10):
-        ser.write('a')
+        ser.write(str.encode('a'))
         print("LED ON")
         time.sleep(0.5)
-        ser.write('0')
+        ser.write(str.encode('0'))
         print("LED OFF")
         time.sleep(0.5)
 
