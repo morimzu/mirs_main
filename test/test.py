bdadd4dba56efa53d883d2815900b4e9a76af5dd
morimzu <mizu.mizupica@gmail.com>
2020-12-23 16:50:53 +0900

    テストモジュールの変更

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import serial
 import time
-from arduino import My_Serial
+from ../arduino import My_Serial
 
 if __name__ == "__main__":
         ser = My_Serial('/dev/ttyACM0', 115200, timeout=50)
